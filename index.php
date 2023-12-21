2b5c09356a6b96c84f13a2af997cc93053e8a811
Ashhabm <140061008+Ashhabm@users.noreply.github.com>
2023-12-21 17:57:38 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,209 @@
+<?php
+session_start();
+require_once 'config.php';
+
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+// // Assuming you have a MySQL database 
+
+    
+    $firstname = $_POST['firstname'];
+    $lastname = $_POST['lastname'];
+    $address = $_POST['address'];
+    $product = $_POST['product'];
+    $price = $_POST['price'];
+
+   
+    
+    print_r( $_SESSION['formData']);
+    
+    // Insert data into 'member' table
+    $insertMember = "INSERT INTO member (first_name,last_name,address) VALUES 
+        ('$firstname','$lastname','$address')";
+
+        if ($conn->query($insertMember) === TRUE) {
+
+            $memberID =$conn->insert_id;
+            
+            $_SESSION['formData'] = array(
+                'firstname' => $firstname,
+                'lastname' => $lastname,
+                'address' => $address,
+                'product' => $product,
+                'price' => $price,
+                'memberid' => $memberID
+            );
+
+            print_r( $_SESSION['formData']);
+
+            // Insert data into 'member_orders' table with the obtained member ID
+            $insertOrder = "INSERT INTO member_orders (product_name,product_price,member_id) VALUES 
+            ( '$product', '$price', '$memberID')";
+
+            if ($conn->query($insertOrder) === TRUE) {
+                header("Location: UpsellOne.php");
+            } else  {
+                echo  $conn->error;
+            }
+        } else  {
+            echo $conn->error;
+    }
+
+}
+
+?>
+
+<!doctype html>
+<html lang="en">
+
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Funnel_test</title>
+        <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+            integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+            crossorigin="anonymous">
+
+            <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
+    </head>
+
+    <body>
+        <center>
+            <h1>Checkout Form </h1>
+        </center>
+        <div class="container mt-5">
+            <form id="submit_form" method="post" onsubmit="submitForm()">
+                <div class="row">
+                    <div class="col-5">
+                        <div>
+                            <label for="" class="form-label">First
+                                Name <sup class="text-danger">*</sup></label>
+                            <input type="text" class="form-control"
+                                id="firstname" name="firstname" >
+                        </div>
+                        <p id="ferror" class="text-danger"></p>
+                    </div>
+                    <div class="col-5">
+                        <div>
+                            <label for="" class="form-label">Last
+                                Name <sup class="text-danger">*</sup></label>
+                            <input type="text" class="form-control"
+                                id="lastname" name="lastname">
+                        </div>
+                        <p id="lerror" class="text-danger"></p>
+                    </div>
+                    <div class="col-2">
+                        <div>
+                            <label for="" class="form-label">YourCart</label>
+                            <div class="form-floating mb-3">
+                                <img src="images/harddisk.webp" alt="" width="70px">
+                                <span class="mx-2" >Hard Disk</span>
+                                <input type="hidden" name="product" value="Hard Disk">
+                                
+                                <p id="perror"></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                <div class="mb-3 col-10">
+                    <label for="" class="form-label">Address <sup class="text-danger">*</sup></label>
+                    <input type="text" class="form-control"
+                        id="address" name="address" placeholder="Enter Full Address">
+                        <p id="aerror" class="text-danger"></p>
+                </div>
+                    <div class="col-2">
+                        <div>
+                            <div class="form-floating mb-3">
+                                <!-- <input type="number" class="form-control"id="price" name="price"placeholder="name@example.com"> -->
+                                <span for="floatingInput">Price</span>
+                                <h6 name="price">4000</h6>
+                                <input type="hidden" name="price" value="4000">
+                                <p id="przerror"></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+             
+             
+
+                                 <hr>
+                 <h3>Payment</h3>
+                 <div class="form-check mt-2">
+                    <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+                    <label class="form-check-label" for="flexRadioDefault1">
+                      Credit Card
+                    </label>
+                  </div>
+                 <div class="form-check mt-1" >
+                    <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+                    <label class="form-check-label" for="flexRadioDefault1">Debit Card</label>
+                  </div>
+                 <div class="form-check mt-1">
+                    <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+                    <label class="form-check-label" for="flexRadioDefault1">Paypal</label>
+                  </div>
+                  <div class="row">
+                    <div class="col-5 my-2">
+                        <div class="mb-3">
+                            <label for="" class="form-label">Name of Card</label>
+                            <input type="text" class="form-control" id="cardname" placeholder="">
+                            <div id="" class="form-text">
+                                Full Name as displed on Card
+                              </div>
+                          </div>
+                    </div>
+                    <div class="col-5 my-2">
+                        <div class="mb-3">
+                            <label for="" class="form-label">Credit Card Number</label>
+                            <input type="text" class="form-control" id="cardnum" placeholder="">
+                          </div>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-2">
+                        <div class="mb-3">
+                            <label for="" class="form-label">Expiration</label>
+                            <input type="text" class="form-control" id="" >
+                          </div>
+                    </div>
+                    <div class="col-2">
+                        <div class="mb-3">
+                            <label for="" class="form-label">CVV</label>
+                            <input type="text" class="form-control" id="cvv" >
+                          </div>
+                    </div>
+                  </div>
+                  <hr>
+                  <div class="d-grid gap-2 ">
+                   <button class="btn btn-primary rounded-3  mx-auto" type="button"  onclick="validateAndSubmit()">Continue to Checkout</button>
+                  </div>
+                  <br>
+            </form>
+        </div>
+        <script src="index.js"></script>
+        
+        <script
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+            crossorigin="anonymous">
+
+                function submitForm() {
+                // Use AJAX to submit the form
+                $.ajax({
+                    url: "index.php",
+                    data: $("#submit_form").serialize(),
+                    success: function(response) {
+                        console.log(response);
+                        
+                    }
+                });
+                }
+    </script>
+ 
+
+
+
+</body>
+</html>
